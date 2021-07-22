db87f66861316cca3e64def85d10836cdbbfce57
Mishari Muqbil <mishari@mishari.net>
2021-07-22 15:12:11 +0700

    Removes leading number with dot (eg. "1. hello")

--- process_text.py
+++ process_text.py
@@ -1,7 +1,12 @@
+import re
 
 def remove_symbols(text):
     symbols = ["●","*","•"]
     output = ''.join([c for c in text if c not in symbols])
+    return output
+
+def remove_number_dot_space(text):
+    output = re.sub(r"\d+\.(\s|$)", "", text)
     return output
 
 def main():
--- tests/test_process_text.py
+++ tests/test_process_text.py
@@ -1,6 +1,9 @@
-from process_text import remove_symbols
+from process_text import *
 
 def test_remove_symbols():
     assert remove_symbols("● สวัสดี") == " สวัสดี"
     assert remove_symbols("สวัส*ดีโลก") == "สวัสดีโลก"
     assert remove_symbols("สวัสดี•โลก") == "สวัสดีโลก"
+
+def test_remove_number_dot_space():
+    assert remove_number_dot_space("1. หวัดดี") == "หวัดดี"
